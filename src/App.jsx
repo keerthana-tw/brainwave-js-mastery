6c741887846dc1b913cf147e1ad52ab019870d06
Keerthana Kasthuril <keerthana.kasthuril@thoughtworks.com>
2025-05-26 18:59:50 +0530

    Design benefits section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import ButtonGradient from './assets/svg/ButtonGradient.jsx';
 import Header from './components/Header.jsx';
 import Hero from './components/Hero.jsx';
+import Benefits from './components/Benefits.jsx';
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
+        <Benefits />
       </div>
       <ButtonGradient />
     </>
